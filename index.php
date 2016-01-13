0c7d6f0bbcd994230b788cc3a3c7f7a9ba4104f0
Alvin Man <alvin.man28@gmail.com>
2016-01-12 16:42:47 -0800

    Playing around with php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title></title>
+</head>
+<body>
+        <?php
+        /**
+         * Created by PhpStorm.
+         * User: alvin
+         * Date: 1/12/2016
+         * Time: 4:09 PM
+         */
+        $name = 'Alvin';
+
+        $what = 'geek';
+
+        $level = 10;
+
+        echo 'Hi, my name is '.$name,'. and I am a level '.$level.'
+        '.$what;
+        ?>
+</body>
+</html>
